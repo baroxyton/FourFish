cb9066f02858156623bfcccd74c97eba49a647c6
Baroxyton <69308275+baroxyton@users.noreply.github.com>
2026-06-26 00:44:37 +0200

    add constructors

--- include/BitBoard.hpp
+++ include/BitBoard.hpp
@@ -1,9 +1,17 @@
 #include <cstddef>
+#include <cstdint>
+#define STATE_t uint64_t
 
 class BitBoard{
   public:
   BitBoard();
-  BitBoard(std::size_t state);
+  BitBoard(STATE_t stateRed, STATE_t stateYellow);
   ~BitBoard();
+
+  private:
+  void joinStates();
+  STATE_t stateRed;
+  STATE_t stateYellow;
+  STATE_t stateCombined;
 };
 
--- src/BitBoard.cpp
+++ src/BitBoard.cpp
@@ -1,14 +1,21 @@
 #include "BitBoard.hpp"
-#include <cstddef>
 
 BitBoard::BitBoard(){
-
+  stateYellow = 0;
+  stateRed = 0;
+  stateCombined = 0;
 };
+
+
+BitBoard::BitBoard(STATE_t stateRed, STATE_t stateYellow){
+  this->stateYellow = stateYellow;
+  this->stateRed = stateRed;
+};
+
+void BitBoard::joinStates(){
+  this->stateCombined = this->stateYellow ^ this->stateRed;
+}
 
 BitBoard::~BitBoard(){
-
-};
-
-BitBoard::BitBoard(std::size_t state){
 
 };
