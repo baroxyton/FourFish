1ee818482c8a10d3a9f54c9eed7ccf12c5736aba
Baroxyton <69308275+baroxyton@users.noreply.github.com>
2026-06-26 17:28:53 +0200

    add header definitions for more functions

--- include/BitBoard.hpp
+++ include/BitBoard.hpp
@@ -2,17 +2,37 @@
 #include <cstdint>
 #define STATE_t uint64_t
 
+#define redField 0
+#define yellowField 1
+#define emptyField 2
+
 class BitBoard{
   public:
   BitBoard();
   BitBoard(STATE_t stateRed, STATE_t stateYellow);
   ~BitBoard();
+
   void clear();
   void setDimensions(const int& width, const int& height);
+  int getField(int x, int y);
+  bool canPlay(int row);
+  void play(int row);
+  bool isRedTurn();
+  bool isOver();
+  bool isDraw();
+  bool hasWon(int color);
+  int getBoard();
+  BitBoard operator>>(int bits) const {
+
+  }
 
 
   private:
   void joinStates();
+  int getRow(int row);
+  int getColumn(int column);
+  int turn;
+  bool over;
   STATE_t stateRed;
   STATE_t stateYellow;
   STATE_t stateCombined;
