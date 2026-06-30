118861a74e58d24502d46b1620d36817188da6ff
Baroxyton <69308275+baroxyton@users.noreply.github.com>
2026-06-30 15:55:22 +0200

    implement staticEval

--- src/Engine.cpp
+++ src/Engine.cpp
@@ -1,6 +1,7 @@
 #include "BitBoard.hpp"
 #include "Engine.hpp"
-#include <bitset>
+#include <bit>
+#include <cmath>
 
 Engine::Engine(){
 
@@ -19,13 +20,39 @@ BitBoard Engine::getBoard(){
   return this->board;
 }
 
-float Engine::staticEvalBoard(const BitBoard& board){
+float Engine::staticEvalSide(const BitBoard& board, int side){
+  STATE_t state = board.getBoard(side);
 
+  // count 2 in a row
+  STATE_t horizontalTwos = state & ((state&~board.firstRowMask)>>1);
+  STATE_t verticalTwos = state & (state>>(board.height));
+  STATE_t NETwos = state & ((state&~board.firstRowMask)>>(board.height+1));
+  STATE_t NWTwos = state & ((state&~board.firstRowMask)>>(board.height-1));
+  
+  int totalTwos = std::popcount(horizontalTwos) + std::popcount(verticalTwos) + std::popcount(NETwos) + std::popcount(NWTwos);
+
+  // count 3 in a row
+  STATE_t horizontalThrees = state & ((state&~board.ABRowMask)>>2);
+  STATE_t verticalThrees = state & (state>>(board.height*2));
+  STATE_t NEThrees = state & ((state&~board.ABRowMask)>>((board.height+1)*2));
+  STATE_t NWThrees = state & ((state&~board.ABRowMask)>>(board.height-1)*2);
+  int totalThrees = std::popcount(horizontalThrees) + std::popcount(verticalThrees) + std::popcount(NEThrees) + std::popcount(NWThrees);
+  
+  int score = totalTwos + totalThrees * 3;
 }
 
-float Engine::staticEval(const BitBoard& boardRed, const BitBoard& boardYellow){
-  return staticEvalBoard(boardRed) - staticEvalBoard(boardYellow);
-}
+float Engine::staticEval(const BitBoard& board){
+  if(board.hasWon(redField)){
+    return INFINITY;
+  }
+  if(board.hasWon((yellowField))){
+    return -INFINITY;
+  }
+  if(board.isDraw()){
+    return 0;
+  }
+  return staticEvalSide(board, redField) - staticEvalSide(board, yellowField);
+};
 
 Engine::~Engine(){
 
